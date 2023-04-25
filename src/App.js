50bca4b5bc2de705d520a230f6a2c74e79bd0f9a
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-25 21:25:14 +0600

    Add CountrySlice.js,store.js and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Details from './components/pages/Details';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/:code" element={<Details />} />
+    </>,
+  ),
+);
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
